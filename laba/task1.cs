2b7cbf84648d6bbb42976a05a66711d3271d9c5c
Khneliuk <arinakhmeluyk@gmail.com>
2026-03-22 13:04:08 +0200

    створено приховані поля та поле для підрахунку

---
+++ laba/task1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace laba_3
+{
+    public class Employee
+    {
+        private readonly int id;
+        private string name;
+        private double salary;
+
+        private static int counter = 0;
+    }
+}
+
